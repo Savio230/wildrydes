d7eff3cdd02eb1df3b9e5bccd8cf1278d2f0dd25
Savio230 <64426979+Savio230@users.noreply.github.com>
2024-11-23 15:48:01 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Jq0RFN3Za', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '64as5frtr9udvfs35j0hi47uvs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_uW9eqX1ez', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4m2kq4ebkka4ue73di1rbluaad', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
